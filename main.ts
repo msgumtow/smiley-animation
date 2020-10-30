d53d6923f69a4cb2dbb9faf7533a56b62da2b0d6
msgumtow <71787320+msgumtow@users.noreply.github.com>
2020-10-30 13:30:46 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+/**
+ * PART 2:
+ * 
+ * Now you'll code the four LEDs that are different to switch back and forth, using "toggle" blocks.
+ * 
+ * Determine the blocks on the smiley and non smiley that are DIFFERENT from each one and plot each one in a toggle block.
+ * 
+ * Put a "pause" block between the first two toggles and the last two toggles.
+ */
+/**
+ * PART 1:
+ * 
+ * Plot the coordinates of only the LEDs that are the SAME in both the smiley and straight faced images.
+ * 
+ * <---Use these as only a reference - do not put them in "on start"
+ * 
+ * Use these plot blocks to plot the coordinates -->
+ * 
+ * Put each plot block in "on start" one by one to see if it's in the right place. I did the first one for you.
+ */
+led.plot(1, 0)
+basic.forever(function () {
+	
+})
